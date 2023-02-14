0d74639efaddbdf3749452b15c23d8c8b104594a
get-me-power <kazuma.a.inagaki@dena.com>
2023-02-14 20:12:57 +0900

    feature: Report BanAsyncTask0001 when method's return type name is "Task"

--- BanAsyncTaskAnalyzer/BanAsyncTaskAnalyzer.cs
+++ BanAsyncTaskAnalyzer/BanAsyncTaskAnalyzer.cs
@@ -11,22 +11,36 @@ namespace BanAsyncTaskAnalyzer;
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
 public class BanAsyncTaskAnalyzer : DiagnosticAnalyzer
 {
-    internal const string DiagnosticId = "BanAsyncTaskAnalyzer0001";
-
-    private static readonly DiagnosticDescriptor s_rule = new DiagnosticDescriptor(
-        id: DiagnosticId,
-        title: "Type name contains lowercase letters",
-        messageFormat: "Type name '{0}' contains lowercase letters",
+    private static readonly DiagnosticDescriptor Rule01 = new DiagnosticDescriptor(
+        id: "BanAsyncTask0001",
+        title: "Banned Task Rule",
+        messageFormat: "Do not use '{0}', Should use UniTask",
         category: "Naming",
         defaultSeverity: DiagnosticSeverity.Warning,
         isEnabledByDefault: true,
         description: "Type names should be all uppercase.");
 
-    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(s_rule);
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule01);
 
     public override void Initialize(AnalysisContext context)
     {
         context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
         context.EnableConcurrentExecution();
+        context.RegisterSymbolAction(AnalyzeAsyncMethod, SymbolKind.Method);
+    }
+
+    private static void AnalyzeAsyncMethod(SymbolAnalysisContext context)
+    {
+        var methodSymbol = (IMethodSymbol)context.Symbol;
+        if (!methodSymbol.IsAsync)
+        {
+            return;
+        }
+
+        if (methodSymbol.ReturnType.Name == "Task")
+        {
+            var diagnostic = Diagnostic.Create(Rule01, methodSymbol.Locations[0], methodSymbol.ReturnType);
+            context.ReportDiagnostic(diagnostic);
+        }
     }
 }
